eef0b558f21b9eed58f32eceefffcd344d9b544b
stark1_5@live.com <stark1_5@live.com>
2020-06-08 20:47:49 +0200

    Libreries en local

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -10,18 +10,16 @@
 
     <!-- CSS only -->
 <link rel="stylesheet"
-  href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" 
-  integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-  crossorigin="anonymous"
+  href="{{asset('css/bootstrap.min.css')}}" 
 >
 
     <link
-      href="https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css"
+      href="{{asset('css/font-awesome.min.css')}}"
       rel="stylesheet"
     />
     <link
       rel="stylesheet"
-      href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+      href="{{asset('css/leaflet.css')}}"
       crossorigin=""
     />
 
@@ -51,7 +49,7 @@
     <div id="map"></div>
 
     <script
-      src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+      src="{{asset('js/leaflet.js')}}"
       crossorigin=""
     ></script>
     <script src="{{asset('js/app.js')}}"></script>
